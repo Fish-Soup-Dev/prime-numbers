83bd3210dfdba03fb03ea53771eca6279beec905
soup <64437861+Fish-game-dev@users.noreply.github.com>
2021-10-07 16:21:45 -0400

    Add files via upload

--- thred.c
+++ thred.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
-    int max = 11;
+    int max = 100000;
+    int prime_numbers = 0;
+    double time_spent = 0.0;
+ 
+    printf("starting... ");
+    clock_t begin = clock();
 
     for (int x = 0; x != max + 1; x++)
     {
@@ -11,9 +17,20 @@ int main()
             int prime = 1;
             for (int i = 2; i != x; i++)
             {   
-                if ((x % i) == 0) { prime = 0; break; }
+                if ((x % i) == 0) 
+                { 
+                    prime = 0; break; 
+                }
             }
-            if (prime == 1) { printf("%i\n",x); }
+            if (prime == 1) 
+            {
+                prime_numbers ++; 
+            }
         }
     }
+
+    clock_t end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("It took %.3f seconds. Found %i prime numbers", time_spent, prime_numbers);
+    return 0;
 }
