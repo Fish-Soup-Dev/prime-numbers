2784eaaca81f0b4b65bebd6a5ce1273113c4f3cd
Fish-game-dev <andrewkawalec164@gmail.com>
2021-10-15 20:02:54 -0400

    !

--- prime number speed test/C/prime_numbers.c
+++ prime number speed test/C/prime_numbers.c
@@ -5,72 +5,91 @@
 #include <unistd.h>
 
 // * retunrns 1 if a number is prime and 0 if its not.
-void* find_prime(void* arg) {
+// ! this is broken.
+// TODO please fix the for loops.
+void* find_prime(void *arg) {
 
-    int value = *(int*)arg;
+    int *val_p = (int *) arg;
     int* result = malloc(sizeof(int));
     int prime = 1;
 
-    // run through all numbers less then input number.
-    for (int i = 2; i <= value / 2; i++) {   
+    int size = sizeof val_p / sizeof *val_p;
 
-        // if number is davisable buy another number that isnt itself then break loop.
-        if (value % i == 0) {
+    for (int x = 2; x <= size; x++) {
 
-            prime = 0;
-            *result = 0;
-            break;
+        // run through all numbers less then input number.
+        for (int i = 2; i <= val_p[x] / 2; i++) {   
+
+            // if number is davisable buy another number that isnt itself then break loop.
+            if (val_p[x] % i == 0) {
+
+                prime = 0;
+                *result += 0;
+                break;
+            }
+        }
+
+        if (prime == 1) {
+
+            *result += 1;
         }
     }
 
-    if (prime == 1) {
-
-        *result = 1;
-    }
-
-    // free data.
-    free(arg);
     return (void*) result;
 }
 
 // * Main loop
 int main(int argc, char* argv[]) {
 
-    int max = 100000;
+    int max = 10;
     int prime_numbers = 0;
     double time_spent = 0.0;
-    int thread_num = 0;
 
     pthread_t th[max];
     int* res;
 
+    int numsA[max / 2];
+    int numsB[max / 2];
+
+    int s = 0;
+
     printf("starting... \n");
     clock_t begin = clock();
 
-    for (int x = 2; x <= max; x++) {   
+    // TODO finsh the array asiment.
+    for (int x = 0; x <= max; x++) {
 
-        int* a = malloc(sizeof(int));
-        *a = x;
+        if (x % 2 != 0) {
+            int numsA[s];
+            s += 1;
+        }
 
-        thread_num += 1;
+    }
 
-        // * create thread with number that we are testing if it is prime.
-        if ( pthread_create(&th[thread_num], NULL, &find_prime, a) != 0) {
+    // * create thread with number that we are testing if it is prime.
+        if ( pthread_create(&th[1], NULL, &find_prime, numsA) != 0) {
             printf("create thread failed\n");
 
         }
-    }
+        if ( pthread_create(&th[2], NULL, &find_prime, numsB) != 0) {
+            printf("create thread failed\n");
+
+        }
     
     // * add all threads awnsers togeather.
-    for (int y = 1; y != thread_num; y++) {
-
-        if ( pthread_join(th[y], (void**) &res) != 0) {
-            printf("join thread failed\n");
-        }
-
-        // add result to total primes found.
-        prime_numbers += *res;
+    if ( pthread_join(th[1], (void**) &res) != 0) {
+        printf("join thread failed\n");
     }
+
+    // add result to total primes found.
+    prime_numbers += *res;
+
+    if ( pthread_join(th[2], (void**) &res) != 0) {
+        printf("join thread failed\n");
+    }
+
+    // add result to total primes found.
+    prime_numbers += *res;
 
     // free data.
     free(res);
