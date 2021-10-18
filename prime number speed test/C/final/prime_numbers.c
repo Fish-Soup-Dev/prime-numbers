1be1e15c9013eb39de5887f39728484586082a95
Fish-game-dev <andrewkawalec164@gmail.com>
2021-10-18 01:34:57 -0400

    yes

--- prime number speed test/C/final/prime_numbers.c
+++ prime number speed test/C/final/prime_numbers.c
@@ -4,28 +4,38 @@
 #include <pthread.h>
 #include <unistd.h>
 
+//  starting amout of numbers to serch through.
 int max = 100000;
 
 // * retunrns number of primes found
 void* find_prime(void *arg) {
 
+    // * weird magic.
     int *val_p = (int *) arg;
     int* result = malloc(sizeof(int));
     int prime = 1, total = 0;
     int smol_num = (max / 2) / 4;
 
+    // * regular prime number finder code.
     for (int x = 1; x <= smol_num; x++) {
+
         for (int i = 2; i <= val_p[x] / 2; i++) {
-            if (val_p[x] % i == 0) {
-                prime = 0;
+
+            if (val_p[x] % i == 0) { 
+
+                prime = 0; 
                 break;
+
             }
+
         }
-        if (prime == 1) {
-            total++;
-        }
+
+        if (prime == 1) { total++; }
+
         prime = 1;
     }
+
+    // * more weird magic.
     *result = total;
     return (void*) result;
 }
@@ -33,6 +43,7 @@ void* find_prime(void *arg) {
 // * Main loop
 int main(int argc, char* argv[]) {
 
+    // * define all starting variables.
     int prime_numbers = 0;
     double time_spent = 0.0;
     pthread_t th[4];
@@ -45,22 +56,23 @@ int main(int argc, char* argv[]) {
     int numsD[smol_num];
     int s = 0, i = 0, j = 0, x = 1;
 
+    // * start clock.
     printf("starting... \n");
     clock_t begin = clock();
 
+    // * remove all even numbers then devide all numbers in to 4 arrys.
     for (x; x <= max; x++) {
-        if (x % 2 != 0) {
-            nums[s] = x;
-            s++;
-        }
+        if (x % 2 != 0) { 
+            nums[s] = x; 
+            s++; 
+        } 
     }
-
-    for(j; j < (max / 2); j += 4) {
-        numsA[i] = nums[j + 0];
-        numsB[i] = nums[j + 1];
-        numsC[i] = nums[j + 2];
-        numsD[i] = nums[j + 3];
-        i++;
+    for (j; j < (max / 2); j += 4) { 
+        numsA[i] = nums[j + 0]; 
+        numsB[i] = nums[j + 1]; 
+        numsC[i] = nums[j + 2]; 
+        numsD[i] = nums[j + 3]; 
+        i++; 
     }
 
     // * create thread with number that we are testing if it is prime.
@@ -79,10 +91,10 @@ int main(int argc, char* argv[]) {
     pthread_join(th[4], (void**) &res);
     prime_numbers += *res;
 
-    free(res);
-
+    // * end clock and print results.
     clock_t end = clock();
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
     printf("It took %.3f seconds. Found %i prime numbers", time_spent, prime_numbers);
+    
     return 0;
 }
