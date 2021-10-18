9ccf2d3f40429a3b2da3bf60d7248fcd43dcc514
Fish-game-dev <andrewkawalec164@gmail.com>
2021-10-18 01:41:26 -0400

    yes

--- prime number speed test/C/working/prime_og.c
+++ prime number speed test/C/working/prime_og.c
@@ -6,29 +6,36 @@ int main()
     int max = 100000;
     int prime_numbers = 0;
     double time_spent = 0.0;
-    int prime = 1, x, i;
+    int prime = 1, x = 1, i = 2, s = 0;
+    int nums[max / 2];
+    int smol_num = max / 2;
     
     printf("starting... \n");
     clock_t begin = clock();
 
-    for (x = 1; x <= max; x++)
-    {   
-        if (x % 2 != 0)
-        {
-            for (i = 2; i <= x / 2; i++)
-            {   
-                if (x % i == 0)
-                {
-                    prime = 0;
-                    break;
-                }
+    for (x; x <= max; x++) {
+        if (x % 2 != 0) { 
+            nums[s] = x; 
+            s++; 
+        } 
+    }
+
+    for (int x = 0; x <= smol_num; x++) {
+
+        for (int i = 2; i <= nums[x] / 2; i++) {
+
+            if (nums[x] % i == 0) { 
+
+                prime = 0; 
+                break;
+
             }
-            if (prime == 1) 
-            {
-                prime_numbers ++; 
-            }
-            prime = 1;
+
         }
+
+        if (prime == 1) { prime_numbers++; }
+
+        prime = 1;
     }
 
     clock_t end = clock();
